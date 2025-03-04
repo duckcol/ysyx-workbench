064c2da4b77b570306cc66ab533c22cd194159d7
tracer-ysyx <tracer@ysyx.org>
2025-03-04 20:48:38 +0800

    > compile NEMU ysyx_22040000 何佳 Linux coladuck-laptop 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:48:38 up  2:58,  1 user,  load average: 0.17, 0.08, 0.08

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,8 +24,6 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-	printf("the content of all regs is as follow:\n");
-
 	int length = sizeof(regs)/sizeof(regs[0]);
 	for(int i = 0; i < length; i++) {
 		printf("%s\n", regs[i]);
