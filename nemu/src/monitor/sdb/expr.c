a34ebf51a2881011005a4306981b8677eb9a1368
tracer-ysyx <tracer@ysyx.org>
2025-03-10 10:04:19 +0800

    > compile NEMU ysyx_22040000 何佳 Linux coladuck-laptop 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:04:19 up 37 min,  1 user,  load average: 0.64, 0.80, 0.53

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -42,7 +42,7 @@ static struct rule {
 	{"\\*", '*'},					// mul
 	{"/", '/'},						// div
   {"==", TK_EQ},        // equal
-	{"12", TK_DIGIT},		// digit
+	{"[[digit]]+", TK_DIGIT},		// digit
 };
 
 #define NR_REGEX ARRLEN(rules)
