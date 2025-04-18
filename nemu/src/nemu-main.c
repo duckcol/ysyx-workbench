160fd36aa0175183169dbd7da5c06a1bf1c8065c
tracer-ysyx <tracer@ysyx.org>
2025-04-18 12:44:00 +0800

    > compile NEMU ysyx_22040000 何佳 Linux coladuck-laptop 6.11.0-19-generic #19~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 17 11:51:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:44:00 up  2:47,  1 user,  load average: 1.01, 1.06, 1.13

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -13,8 +13,58 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
-#include "nemu-main.h"
-//	all needed function and definition is moved "nemu-main.h"
+#include "debug.h"
+#include <common.h>
+#include <stdio.h>
+#include <string.h>
+#include "monitor/sdb/sdb.h"
+
+void init_monitor(int, char *[]);
+void am_init_monitor();
+void engine_start();
+int is_exit_status_bad();
+
+inline int readin_expr_test() {
+	FILE *file = fopen("/home/coladuck/ysyx-workbench/nemu/tools/gen-expr/input", "r");
+	FILE *pass = fopen("/home/coladuck/ysyx-workbench/nemu/tools/gen-expr/pass", "w");
+	FILE *fail = fopen("/home/coladuck/ysyx-workbench/nemu/tools/gen-expr/fail", "w");
+	Assert(file != NULL, "input file open failed");
+	Assert(pass != NULL, "pass file open failed");
+	Assert(fail != NULL, "fail file open failed");
+
+	char line[65536 + 128];
+	int pass_cnt = 0; int fail_cnt = 0; int input_line_num = 0;
+	while (fgets(line, sizeof(line), file) != NULL) {
+		//	deal with line change
+		long len = strlen(line);	
+		if (len > 0 && line[len - 1] == '\n') {
+			line[len - 1] = '\0';	
+		}
+
+		//	deal with each line 
+		//	store and change the number
+		input_line_num++;
+		word_t num; char *expression;
+		num = strtoull(line , &expression, 10);
+		Assert(expression != line, "first character is not digit");
+		bool success;
+		word_t result = expr(expression, &success);
+		if (result == num) {
+			pass_cnt++;
+			fprintf(pass, "Location: %d; Num:"FMT_WORD"; Result:"FMT_WORD"; TokensSuccess:%d; Expression:%s;\n", input_line_num, num, result, success, expression);
+		} else {
+			fail_cnt++;
+			fprintf(fail, "Location: %d; Num:"FMT_WORD"; Result:"FMT_WORD"; TokensSuccess:%d; Expression:%s;\n",input_line_num, num, result, success, expression);
+		}
+	}
+	CORRECT("pass example num:%d\n", pass_cnt);
+	WARN("failed example num:%d\n", fail_cnt);
+
+	//	some check
+	Assert(ferror(file) == 0, "file reading error");
+	fclose(file);fclose(pass);fclose(fail);
+	return 0;
+};
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
