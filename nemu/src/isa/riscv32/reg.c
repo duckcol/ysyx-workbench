2263b4c8c216ba7c1f2bfb3d58afdebb66ada413
tracer-ysyx <tracer@ysyx.org>
2025-04-18 17:50:03 +0800

    > compile NEMU ysyx_22040000 何佳 Linux coladuck-laptop 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:50:03 up  3:41,  1 user,  load average: 0.24, 0.21, 0.18

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -46,9 +46,10 @@ void isa_reg_display() {
 
 word_t isa_reg_str2val(const char *s, bool *success) {
 	int length = sizeof(regs)/sizeof(regs[0]);
-	for (int i = 0; i < length; i++) {
+	for (int i = 1; i < length; i++) {
 		*success = (strncmp(s+1, regs[i], 11) == 0) ? true : false;
 		if (*success == true) break;
 	}
+	*success = (strncmp(s, regs[0], 11) == 0) ? true : *success;
   return 0;
 }
