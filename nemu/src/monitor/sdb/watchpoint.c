4635adec381df7ef63234a8aaec760f85742c8e5
tracer-ysyx <tracer@ysyx.org>
2025-07-28 15:41:05 +0800

    > compile NEMU ysyx_22040000 何佳 Linux coladuck-laptop 6.14.0-24-generic #24~24.04.3-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul  7 16:39:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:41:05 up  2:50,  1 user,  load average: 0.03, 0.05, 0.08

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -119,8 +119,8 @@ void test_new_and_free_WP(){
 	printf_the_free_WP_list();
 	printf_the_used_WP_list();
 
-	free(head);
 	printf("free 1 WP, the list:\n");
+	free(head);
 	printf_the_free_WP_list();
 	printf_the_used_WP_list();
 
