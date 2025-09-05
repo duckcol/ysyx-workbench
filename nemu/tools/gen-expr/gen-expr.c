51a936381f75b4f52b9e18c74730241edd931ad3
tracer-ysyx <tracer@ysyx.org>
2025-09-05 11:12:38 +0800

    > compile NEMU ysyx_22040000 何佳 Linux coladuck-laptop 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:12:38 up  2:22,  1 user,  load average: 0.20, 0.26, 0.20

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -49,7 +49,7 @@ static void gen_rand_expr() {
   case 0:
     // gen_num
     INSERT_BLANK;
-    sprintf(BUFEND, "%u", (uint32_t)choose(1000));
+    sprintf(BUFEND, "%uul", (uint32_t)choose(1000));
     INSERT_BLANK;
     break;
   case 1:
