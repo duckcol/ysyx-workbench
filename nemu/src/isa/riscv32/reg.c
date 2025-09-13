d5d7939efce8d70b2f2b89c585d4a526afaa9590
tracer-ysyx <tracer@ysyx.org>
2025-09-13 13:29:48 +0800

    > compile NEMU ysyx_22040000 何佳 Linux coladuck-laptop 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:29:48 up  4:37,  1 user,  load average: 0.43, 0.40, 0.32

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -44,23 +44,29 @@ void isa_reg_display() {
 word_t isa_reg_str2val(const char *s, bool *success) {
   int length = sizeof(regs) / sizeof(regs[0]);
   word_t value;
+
   // search for reg name except $0 and pc
   for (int i = 1; i < length; i++) {
     *success = (strncmp(s + 1, regs[i], 11) == 0) ? true : false;
     if (*success == true) {
       value = gpr(i);
-      break;
+      Info("reg found!");
+      return value;
+      break; //  maybe don't needed
     }
   }
 
   // search for reg $0
   *success = (strncmp(s, regs[0], 11) == 0) ? true : *success;
   value = success ? gpr(0) : 0;
+  if (*success) {
+    Info("reg found!");
+    return value;
+  }
 
   // search for reg pc
   *success = (strncmp(s + 1, "pc", 11) == 0) ? true : *success;
   value = success ? cpu.pc : 0;
-
   if (*success) {
     Info("reg found!");
     return value;
