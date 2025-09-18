1f6618ee264acb479a555900c8abbe8523653513
HeJia(coladuck/duckcol) <HJ191@qq.com>
2025-09-18 15:26:16 +0800

    add type J and U and untest TODO comment

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -14,6 +14,7 @@
  ***************************************************************************************/
 
 #include "local-include/reg.h"
+#include "macro.h"
 #include <cpu/cpu.h>
 #include <cpu/decode.h>
 #include <cpu/ifetch.h>
@@ -27,6 +28,8 @@ enum {
   TYPE_I,
   TYPE_U,
   TYPE_S,
+  TYPE_B,
+  TYPE_J,
   TYPE_N, // none
 };
 
@@ -50,6 +53,18 @@ enum {
   do {                                                                         \
     *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7);                   \
   } while (0)
+// TODO: immB() and immJ() untested
+#define immB()                                                                 \
+  do {                                                                         \
+    *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 7, 7) << 11) |          \
+           (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1);                     \
+  } while (0)
+#define immJ()                                                                 \
+  do {                                                                         \
+    *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 19, 12) << 12) |        \
+           (BITS(i, 20, 20) << 11) | (BITS(i, 30, 25) << 5) |                  \
+           (BITS(i, 24, 21) << 1);                                             \
+  } while (0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2,
                            word_t *imm, int type) {
@@ -58,6 +73,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2,
   int rs2 = BITS(i, 24, 20);
   *rd = BITS(i, 11, 7);
   switch (type) {
+  // TODO: TYPE_R, TYPE_B, TYPE_J untested
   case TYPE_R:
     src1R();
     src2R();
@@ -73,6 +89,14 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2,
     src1R();
     src2R();
     immS();
+    break;
+  case TYPE_B:
+    src1R();
+    src2R();
+    immB();
+    break;
+  case TYPE_J:
+    immJ();
     break;
   }
 }
