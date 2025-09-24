3b4723ea3427c7b2c0e2b41a9ec66de78d6c1402
tracer-ysyx <tracer@ysyx.org>
2025-09-24 14:49:19 +0800

    > compile NEMU ysyx_22040000 何佳 Linux coladuck-laptop 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:49:19 up  4:30,  1 user,  load average: 0.55, 0.43, 0.37

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -236,11 +236,11 @@ static int decode_exec(Decode *s) {
           WARN("exec inst \"mulh\": convert src2 uint32_t " FMT_WORD
                " to int32_t %d",
                src2, (int32_t)src2);
-          int64_t tmp = (uint64_t)src1 * (uint64_t)src2;
+          uint64_t tmp = (uint64_t)src1 * (uint64_t)src2;
           Info("mulh: full result is "
                "0x%016" PRIx64 ", high 32 bits is " FMT_WORD "",
                tmp, (word_t)(tmp >> 32) & 0xfffffff);
-          R(rd) = (tmp >> 32) & 0xffffffff);
+          R(rd) = (word_t)(tmp >> 32) & 0xffffffff);
   INSTPAT("0000001 ????? ????? 010 ????? 01100 11", mulhsu, R,
           R(rd) = BITS((int64_t)((int32_t)src1 * (uint32_t)src2), 63, 32));
   INSTPAT("0000001 ????? ????? 011 ????? 01100 11", mulhu, R,
