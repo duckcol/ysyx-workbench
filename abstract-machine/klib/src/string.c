1719f34839ccc3ddf50e996d542a1778d2191716
tracer-ysyx <tracer@ysyx.org>
2025-10-26 13:14:53 +0800

    > compile NEMU ysyx_25090241 何佳 Linux coladuck-laptop 6.14.0-33-generic #33~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 17:02:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:14:53 up 24 min,  1 user,  load average: 0.53, 0.32, 0.22

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,44 +1,49 @@
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdint.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t i = 0;
+  while (*(s + i) != '\0') {
+    i++;
+  }
+  return i;
 }
 
-char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
-}
+char *strcpy(char *dst, const char *src) { panic("Not implemented"); }
 
 char *strncpy(char *dst, const char *src, size_t n) {
   panic("Not implemented");
 }
 
-char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
-}
+char *strcat(char *dst, const char *src) { panic("Not implemented"); }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  /*  compare the first difference byte (or char)
+   *  in the same position of s1 and s2
+   */
+  for (const unsigned char *p1 = (unsigned char *)s1, *p2 = (unsigned char *)s2;
+       *p1 && *p2; p1++, p2++) {
+    if (*p1 != *p2) {
+      return (int)(*p1 - *p2);
+    }
+  }
+  return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
   panic("Not implemented");
 }
 
-void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
-}
+void *memset(void *s, int c, size_t n) { panic("Not implemented"); }
 
 void *memmove(void *dst, const void *src, size_t n) {
   panic("Not implemented");
 }
 
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
-}
+void *memcpy(void *out, const void *in, size_t n) { panic("Not implemented"); }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
   panic("Not implemented");
