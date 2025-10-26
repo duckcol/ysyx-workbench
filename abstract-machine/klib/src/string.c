65f285d01a99863d855cf955b22993e7be4d90a2
tracer-ysyx <tracer@ysyx.org>
2025-10-26 15:11:30 +0800

    > compile NEMU ysyx_25090241 何佳 Linux coladuck-laptop 6.14.0-33-generic #33~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 17:02:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:11:30 up  2:20,  1 user,  load average: 0.24, 0.19, 0.12

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,6 +1,6 @@
 #include <klib-macros.h>
 #include <klib.h>
-#include <stdint.h>
+#include <stddef.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
@@ -12,13 +12,28 @@ size_t strlen(const char *s) {
   return i;
 }
 
-char *strcpy(char *dst, const char *src) { panic("Not implemented"); }
+void *mempcpy(void *out, const void *in, size_t n);
+char *stpcpy(char *dst, const char *src) {
+  char *p;
+
+  p = mempcpy(dst, src, strlen(src));
+  *p = '\0';
+
+  return p;
+}
+char *strcpy(char *dst, const char *src) {
+  stpcpy(dst, src);
+  return dst;
+}
 
 char *strncpy(char *dst, const char *src, size_t n) {
   panic("Not implemented");
 }
 
-char *strcat(char *dst, const char *src) { panic("Not implemented"); }
+char *strcat(char *dst, const char *src) {
+  stpcpy(dst + strlen(dst), src);
+  return dst;
+}
 
 int strcmp(const char *s1, const char *s2) {
   /*  compare the first difference byte (or char)
@@ -37,16 +52,40 @@ int strncmp(const char *s1, const char *s2, size_t n) {
   panic("Not implemented");
 }
 
-void *memset(void *s, int c, size_t n) { panic("Not implemented"); }
+void *memset(void *s, int c, size_t n) {
+  for (size_t i = 0; i < n; i++) {
+    *((unsigned char *)s + i) = c;
+  }
+  return s;
+}
 
 void *memmove(void *dst, const void *src, size_t n) {
   panic("Not implemented");
 }
 
-void *memcpy(void *out, const void *in, size_t n) { panic("Not implemented"); }
+void *memcpy(void *out, const void *in, size_t n) {
+  for (size_t i = 0; i < n; i++) {
+    *(unsigned char *)(out + i) = *(unsigned char *)(in + i);
+  }
+  return out;
+}
+
+void *mempcpy(void *out, const void *in, size_t n) {
+  return (memcpy(out, in, n) + n);
+}
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  /*  compare the first difference byte (or char)
+   *  in the same position of s1 and s2 in s1+n or s2+n
+   */
+  unsigned char *p1, *p2;
+  for (size_t i = 0; i < n; i++) {
+    p1 = (unsigned char *)s1 + i;
+    p2 = (unsigned char *)s2 + i;
+    if (*p1 != *p2)
+      return (int)(*p1 - *p2);
+  }
+  return 0;
 }
 
 #endif
