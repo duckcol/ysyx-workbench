26da6d15c0e2fb2f3af1330a92afe52b3e4fabab
HeJia(coladuck/duckcol) <HJ191@qq.com>
2025-10-26 15:14:32 +0800

    implemente part of klib/string.c to run string.c in cpu-tests

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,15 +1,28 @@
-#include <klib.h>
 #include <klib-macros.h>
-#include <stdint.h>
+#include <klib.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t i = 0;
+  while (*(s + i) != '\0') {
+    i++;
+  }
+  return i;
 }
 
+void *mempcpy(void *out, const void *in, size_t n);
+char *stpcpy(char *dst, const char *src) {
+  char *p;
+
+  p = mempcpy(dst, src, strlen(src));
+  *p = '\0';
+
+  return p;
+}
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  stpcpy(dst, src);
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +30,21 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  stpcpy(dst + strlen(dst), src);
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  /*  compare the first difference byte (or char)
+   *  in the same position of s1 and s2
+   */
+  for (const unsigned char *p1 = (unsigned char *)s1, *p2 = (unsigned char *)s2;
+       *p1 && *p2; p1++, p2++) {
+    if (*p1 != *p2) {
+      return (int)(*p1 - *p2);
+    }
+  }
+  return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +52,10 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  for (size_t i = 0; i < n; i++) {
+    *((unsigned char *)s + i) = c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -37,11 +63,28 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  for (size_t i = 0; i < n; i++) {
+    *(unsigned char *)(out + i) = *(unsigned char *)(in + i);
+  }
+  return out;
+}
+
+void *mempcpy(void *out, const void *in, size_t n) {
+  return (memcpy(out, in, n) + n);
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  /*  compare the first difference byte (or char)
+   *  in the same position of s1 and s2 in s1+n or s2+n
+   */
+  unsigned char *p1, *p2;
+  for (size_t i = 0; i < n; i++) {
+    p1 = (unsigned char *)s1 + i;
+    p2 = (unsigned char *)s2 + i;
+    if (*p1 != *p2)
+      return (int)(*p1 - *p2);
+  }
+  return 0;
 }
 
 #endif
