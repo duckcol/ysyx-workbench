cf04422a6ad876ac56992b63ed5eb4bfba4e35dd
tracer-ysyx <tracer@ysyx.org>
2025-10-30 22:25:54 +0800

    > compile NEMU ysyx_25090241 何佳 Linux coladuck-laptop 6.14.0-33-generic #33~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 17:02:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:25:54 up  2:24,  1 user,  load average: 0.11, 0.17, 0.19

--- nemu/src/monitor/sdb/mtrace.c
+++ nemu/src/monitor/sdb/mtrace.c
@@ -25,8 +25,10 @@ int push_mem_trace(paddr_t addr, int type, word_t data) {
     sprintf(mt.ringbuff[mt.wp],
             "type: write, addr:" FMT_PADDR ", data:" FMT_WORD " ", addr, data);
   else if (type == 1)
-    sprintf(mt.ringbuff[mt.wp],
-            "type: read , addr:" FMT_PADDR ", data:" FMT_WORD " ", addr, data);
+    // sprintf(mt.ringbuff[mt.wp],
+    //         "type: read , addr:" FMT_PADDR ", data:" FMT_WORD " ", addr,
+    //         data);
+    sprintf(mt.ringbuff[mt.wp], "test %d", mt.rp);
   wp_update;
   return 0;
 }
