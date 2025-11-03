7f209f78f2ae2ea95791bef4988a015146d2272c
tracer-ysyx <tracer@ysyx.org>
2025-11-03 17:36:25 +0800

    > compile NEMU ysyx_25090241 何佳 Linux coladuck-laptop 6.14.0-33-generic #33~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 17:02:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:36:25 up 51 min,  1 user,  load average: 0.11, 0.18, 0.18

--- nemu/src/monitor/sdb/ftrace.c
+++ nemu/src/monitor/sdb/ftrace.c
@@ -22,23 +22,42 @@ void init_ftrace(const char *elf_file) {
 
   Log("the strtabndx: %d", elf_header.e_shstrndx);
 
-  //  find .symtab section
+  //  find .symtab .strtab and shstrtab section header
   fseek(fp, elf_header.e_shoff, SEEK_SET);
-  // Elf32_Shdr symtab_shdr, strtab_shdr;
+  Elf32_Shdr symtab_shdr, strtab_shdr, shstrtab_shdr;
   for (int i = 0; i < elf_header.e_shnum; i++) {
     Elf32_Shdr section_header;
     ret = fread(&section_header, 1, sizeof(Elf32_Shdr), fp);
-    //  to find section which name == 'symtab' and 'strtab'
     Assert(ret != 0, "read section error");
+
+    //  to find section which name == 'symtab' and 'strtab'
     if (section_header.sh_type == SHT_SYMTAB) {
       Log("find symtab when shnum == %d", i);
-      // symtab_shdr = section_header;
+      symtab_shdr = section_header;
     } else if (section_header.sh_type == SHT_STRTAB &&
                i != elf_header.e_shstrndx) {
       Log("find strtab when shnum == %d", i);
-      // strtab_shdr = section_header;
+      strtab_shdr = section_header;
     } else if (i == elf_header.e_shstrndx) {
       Log("find shstrtab when shnum == %d", i);
+      shstrtab_shdr = section_header;
     }
   }
+
+  //  read in .symtab .strtab and .shstrtab data
+  fseek(fp, symtab_shdr.sh_offset, SEEK_SET);
+  Elf32_Sym *symtab = malloc(symtab_shdr.sh_size);
+  ret = fread(symtab, 1, symtab_shdr.sh_size, fp);
+  Assert(ret, "symtab readin error");
+
+  fseek(fp, strtab_shdr.sh_offset, SEEK_SET);
+  char *strtab = malloc(strtab_shdr.sh_size);
+  ret = fread(strtab, 1, strtab_shdr.sh_size, fp);
+  Assert(ret, "strtab readin error");
+
+  fseek(fp, shstrtab_shdr.sh_offset, SEEK_SET);
+  char *shstrtab = malloc(shstrtab_shdr.sh_size);
+  ret = fread(shstrtab, 1, shstrtab_shdr.sh_size, fp);
+  Assert(ret, "shstrtab readin error");
+  Log("first str in shstrtab:%s", shstrtab + 1);
 }
