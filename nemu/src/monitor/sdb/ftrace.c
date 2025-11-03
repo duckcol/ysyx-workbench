5a24e4322863972d9b595e71cd6f4c905884542e
tracer-ysyx <tracer@ysyx.org>
2025-11-03 13:51:16 +0800

    > compile NEMU ysyx_25090241 何佳 Linux coladuck-laptop 6.14.0-33-generic #33~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 17:02:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:51:16 up  1:03,  1 user,  load average: 0.20, 0.21, 0.19

--- nemu/src/monitor/sdb/ftrace.c
+++ nemu/src/monitor/sdb/ftrace.c
@@ -1,9 +1,37 @@
 #include "sdb.h"
+#include <elf.h>
 
 void init_ftrace(const char *elf_file) {
+  //  check if there is an elf file
   if (elf_file == NULL) {
     Log("no elf file input, ftrace malfunction");
     return;
   }
+
+  //  read in elf file
   Log("The elf file is %s", elf_file);
+  FILE *fp = fopen(elf_file, "rb");
+  Assert(fp, "Can't open elf_file '%s'", elf_file);
+
+  //  read elf header
+  Elf32_Ehdr elf_header;
+  int ret;
+  ret = fread(&elf_header, 1, sizeof(Elf32_Ehdr), fp);
+  Assert(ret == 1, "read elf header error");
+
+  //  find .symtab section
+  fseek(fp, elf_header.e_shoff, SEEK_SET);
+  // Elf32_Shdr symtab_shdr, strtab_shdr;
+  for (int i = 0; i < elf_header.e_shnum; i++) {
+    Elf32_Shdr section_header;
+    ret = fread(&section_header, 1, sizeof(Elf32_Shdr), fp);
+    Assert(ret == 1, "read section error");
+    if (section_header.sh_type == SHT_SYMTAB) {
+      Log("find symtab");
+      // symtab_shdr = section_header;
+    } else if (section_header.sh_type == SHT_STRTAB) {
+      Log("find strtab");
+      // strtab_shdr = section_header;
+    }
+  }
 }
